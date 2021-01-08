619fc73da74d54beca76d9dd159d25927bd7ef03
éHappyCeng <mutlu.m38@gmail.com>
2021-01-08 20:47:25 +0300

    Add project files.

---
+++ Random/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random randomNumber = new Random();
+            int num = randomNumber.Next(); // random number
+
+            num = randomNumber.Next(50);
+
+            num = randomNumber.Next(98, 102);
+
+            Console.WriteLine(num.ToString());
+
+            int ascii = randomNumber.Next(65, 91); // random character
+            char character = Convert.ToChar(ascii);
+
+            Console.WriteLine(character.ToString());
+        }
+    }
+}
